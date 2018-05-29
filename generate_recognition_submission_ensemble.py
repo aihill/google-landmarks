f1a108d04030eb6d9561c9ba61e8c28d8670ae8f
artyom <cppg@cppg.co.uk>
2018-05-29 20:00:03 +0100

    added debug visualization for predictions in recognition

--- generate_recognition_submission_ensemble.py
+++ generate_recognition_submission_ensemble.py
@@ -3,7 +3,7 @@
 Uses ensemble of networks. """
 
 from typing import *
-import math, os, sys
+import math, os, shutil, sys
 from collections import defaultdict
 from shutil import copyfile
 import numpy as np,  pandas as pd       # type: ignore
@@ -11,7 +11,39 @@ from tqdm import tqdm                   # type: ignore
 from matplotlib import pyplot as plt    # type: ignore
 
 NpArray = Any
+
 NON_LANDMARK_CLASSIFIER     = "experiments/no_class_1/pred.npz"
+ENABLE_DEBUGGING            = True
+
+def prepare_debugging(debug_dir: str) -> DefaultDict[int, List[str]]:
+    print("removing old debug data")
+
+    if os.path.exists(debug_dir):
+        shutil.rmtree(debug_dir)
+
+    os.makedirs(debug_dir)
+
+    print("reading csv...")
+    csv_path = "data/train.csv"
+    data = pd.read_csv(csv_path)
+    x: NpArray = data["id"].values
+    y: NpArray = data["landmark_id"].values
+    print("len(x)", len(x), "y.shape", y.shape)
+    print()
+
+    print("filtering data...")
+    ok = [os.path.exists("data/train/" + p + ".jpg") for p in tqdm(x)]
+    x, y = x[ok], y[ok]
+    print("after filtering: len(x)", len(x), "y.shape", y.shape)
+
+    print("parsing classes")
+    classes: DefaultDict[int, List[str]] = defaultdict(list)
+
+    for filename, class_ in zip(x, y):
+        if len(classes[class_]) <= 20:
+            classes[class_].append(filename)
+
+    return classes
 
 def load_test_data(path: str) -> List[str]:
     """ Loads CSV files into memory. """
@@ -93,12 +125,37 @@ if __name__ == "__main__":
     # generate results
     ###########################################################################
     data = defaultdict(str)
-    for i in range(images.size):
-        name = os.path.splitext(image_list[i])[0]
+
+    if ENABLE_DEBUGGING:
+        root_dir = os.path.abspath(os.path.dirname(__file__)) + "/"
+        debug_dir = root_dir + "experiments/recognition_stats/"
+
+        classes_dict = prepare_debugging(debug_dir)
+
+    print("processing test set")
+
+    for i in tqdm(range(images.size)):
+        test_img = os.path.splitext(image_list[i])[0]
         class_, conf = merge_results(all_classes, all_confidences, i)
 
-        if name not in non_landmarks:
-            data[name] = "%d %f" % (class_, conf)
+        if test_img not in non_landmarks:
+            data[test_img] = "%d %f" % (class_, conf)
+
+            if ENABLE_DEBUGGING:
+                directory = debug_dir + str(class_) + "/"
+
+                if not os.path.exists(directory):
+                    # create directory, copy a few originals
+                    os.mkdir(directory)
+                    classes = classes_dict[class_]
+
+                    for filename in classes:
+                        os.symlink(root_dir + "data/train/" + filename + ".jpg",
+                                   directory + "orig_" + filename + ".jpg")
+
+                # copy test sample, mention probabilities
+                os.symlink(root_dir + "data/test/" + test_img + ".jpg",
+                           directory + test_img + "_" + str(conf) + ".jpg")
 
     x_test = load_test_data("recognition/test.csv")
     values = [""] * len(x_test)
